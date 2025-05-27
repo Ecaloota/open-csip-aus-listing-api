24a110f7961daf0a91d505b464b10a0117d794ce
mitchell38 <mitchell@thaum.io>
2025-05-27 22:02:30 +1000

    implement test session dependency on the v1 router

--- open_cec_api/api/v1/router.py
+++ open_cec_api/api/v1/router.py
@@ -1,16 +1,26 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from open_cec_api.api.auth import check_key_header
+from open_cec_api.services.database.db import ensure_session
 
-# TODO add database session dependency
-router = APIRouter(dependencies=[Depends(check_key_header)])
+HeaderDependency = Depends(check_key_header)
+SessionDependency = Annotated[Session, Depends(ensure_session)]
+
+# We cannot pass the SessionDependency directly to the APIRouter
+router = APIRouter(dependencies=[HeaderDependency])
 
 
 # TODO remove this endpoint when the API is ready
 @router.get("/")
-def status_check() -> str:
+def status_check(session: SessionDependency) -> str:
     """
     Checks the health of a project.
     """
+
+    # TODO remove this
+    print(f"Session dependency {session}")
 
     return "OK"
